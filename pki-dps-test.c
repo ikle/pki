8404be96e48e05b1618caf09f949f040e82c4b0c
Alexei A. Smekalkine <ikle@ikle.ru>
2022-07-04 15:50:22 +0300

    pki: add DP scan usage sample

---
+++ pki-dps-test.c
@@ -0,0 +1,30 @@
+/*
+ * PKI DPs test
+ *
+ * Copyright (c) 2016-2022 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <stdio.h>
+
+#include "pki.h"
+
+static int dp_cb (const X509 *ca, const char *uri, void *cookie)
+{
+	char *name = X509_NAME_oneline (X509_get_subject_name (ca), NULL, 0);
+
+	if (name == NULL)
+		return 0;
+
+	printf ("%s:\n\t%s\n", name, uri);
+
+	OPENSSL_free (name);
+	return 0;
+}
+
+int main (int argc, char *argv[])
+{
+	pki_scan_dps (NULL, NULL, dp_cb, NULL);
+	return 0;
+}
