f11787f78b06206007bd9145f9ce7ae53b70ddfd
Alexei A. Smekalkine <ikle@ikle.ru>
2022-07-09 21:41:51 +0300

    pki-dps-test: use RFC 2253 format to print DN

--- pki-dps-test.c
+++ pki-dps-test.c
@@ -22,14 +22,14 @@ static void show_crl (const X509_CRL *crl)
 
 static int dp_cb (const X509 *ca, const char *uri, void *cookie)
 {
-	char *name = X509_NAME_oneline (X509_get_subject_name (ca), NULL, 0);
+	X509_NAME *name = X509_get_subject_name (ca);
 	X509_CRL *crl;
 
 	if (name == NULL)
 		return 0;
 
-	printf ("%s:\n\t%s\n", name, uri);
-	OPENSSL_free (name);
+	X509_NAME_print_ex_fp (stdout, name, 0, XN_FLAG_RFC2253);
+	printf (":\n\t%s\n", uri);
 
 	if (!pki_fetch (uri, 0, pki_crl_cb, &crl))
 		return 0;
