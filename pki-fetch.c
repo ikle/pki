8e7e512d21cb57af948f7a0ee9590f02de5a659e
Alexei A. Smekalkine <ikle@ikle.ru>
2022-07-09 15:03:49 +0300

    pki-fetch: add generic helper

---
+++ pki-fetch.c
@@ -0,0 +1,19 @@
+/*
+ * PKI Fetch helpers
+ *
+ * Copyright (c) 2020-2022 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <string.h>
+
+#include "pki-fetch.h"
+
+int pki_fetch (const char *uri, int limit, pki_data_cb cb, void *cookie)
+{
+	if (strncmp (uri, "ldap", 4) == 0)
+		return pki_ldap_fetch (uri, limit, cb, cookie);
+
+	return 0;
+}
--- pki-fetch.h
+++ pki-fetch.h
@@ -15,4 +15,6 @@ typedef int pki_data_cb (const void *data, size_t len, void *cookie);
 
 int pki_ldap_fetch (const char *uri, int limit, pki_data_cb cb, void *cookie);
 
+int pki_fetch (const char *uri, int limit, pki_data_cb cb, void *cookie);
+
 #endif  /* PKI_FETCH_H */
