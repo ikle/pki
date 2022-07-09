97f3111b41e8c923c183067b989ce6f808205b21
Alexei A. Smekalkine <ikle@ikle.ru>
2022-07-10 02:57:36 +0300

    pki-dps-test: show CRL update times if any

--- pki-dps-test.c
+++ pki-dps-test.c
@@ -11,13 +11,33 @@
 #include "pki.h"
 #include "pki-fetch.h"
 
+static void ASN1_TIME_show (const char *prefix, const ASN1_TIME *s, FILE *to)
+{
+	BIO *b;
+
+	if (s == NULL || (b = BIO_new_fp (to, BIO_NOCLOSE)) == NULL)
+		return;
+
+	fputs (prefix, to);
+	ASN1_TIME_print (b, s);
+	fputc ('\n', to);
+
+	BIO_free (b);
+}
+
 static void show_crl (const X509_CRL *crl)
 {
 	X509_NAME *name = X509_CRL_get_issuer (crl);
+	const ASN1_TIME *last = X509_CRL_get0_lastUpdate (crl);
+	const ASN1_TIME *next = X509_CRL_get0_nextUpdate (crl);
 
 	printf ("\tCRL issuer: ");
 	X509_NAME_print_ex_fp (stdout, name, 0, XN_FLAG_RFC2253);
+
 	printf ("\n");
+
+	ASN1_TIME_show ("\tlast update = ", last, stdout);
+	ASN1_TIME_show ("\tnext update = ", next, stdout);
 }
 
 static int dp_cb (const X509 *ca, const char *uri, void *cookie)
