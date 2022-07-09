1afba27cfc236feb9c526077c5689a834ba8042f
Alexei A. Smekalkine <ikle@ikle.ru>
2022-07-09 15:21:25 +0300

    pki-fetch: add CRL fetch callback

--- pki-fetch.c
+++ pki-fetch.c
@@ -8,7 +8,22 @@
 
 #include <string.h>
 
+#include <openssl/x509.h>
+
 #include "pki-fetch.h"
+
+int pki_crl_cb (const void *data, size_t len, void *cookie)
+{
+	const unsigned char *p = data;
+	X509_CRL **ret = cookie, *c;
+
+	if ((c = d2i_X509_CRL (NULL, &p, len)) != NULL) {
+		*ret = c;
+		return 1;
+	}
+
+	return 0;
+}
 
 int pki_fetch (const char *uri, int limit, pki_data_cb cb, void *cookie)
 {
--- pki-fetch.h
+++ pki-fetch.h
@@ -16,6 +16,9 @@ typedef int pki_data_cb (const void *data, size_t len, void *cookie);
 int pki_http_fetch (const char *uri, int limit, pki_data_cb cb, void *cookie);
 int pki_ldap_fetch (const char *uri, int limit, pki_data_cb cb, void *cookie);
 
+/* cookie = X509_CRL ** */
+int pki_crl_cb (const void *data, size_t len, void *cookie);
+
 int pki_fetch (const char *uri, int limit, pki_data_cb cb, void *cookie);
 
 #endif  /* PKI_FETCH_H */
